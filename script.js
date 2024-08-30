44ed0b7f4574b3a5fea237702ccaff3c550571e1
Herron182 <jamesherron182@gmail.com>
2024-08-30 19:33:13 +0100

    level up and button hide

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-let xp = 0;
+let xp = 0; //set back to 0 after testing
 let health = 100;
-let gold = 50;
+let gold = 50; //set back to 50 after testing
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-
+let playerLevel = 1;
+let xpNeeded = 15; // XP needed to reach level 2
+const maxLevel = 10; // Max level cap
+                                                                                                 
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
+const playerLevelText = document.querySelector("#playerLevelText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
@@ -42,80 +47,322 @@ const monsters = [
 const locations = [
   {
     name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"Store\"."
+    "button text": ["Go to store", "Go to cave", "Fight dragon", "Level Up!"],
+    "button functions": [goStore, goCave, fightDragon, levelUp],
+    text: "You are in the town square. You see a sign that says \"Store\".",
+
+    // Visibility tracking for each button (true = visible, false = hidden)
+    "button visibility": [true, true, true, true],
+
+    // Method to hide a specific button by index
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+
+    // Method to show a specific button by index
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+
+    // Method to render buttons considering visibility
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = ''; // Clear previous buttons
+
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) { // Only render visible buttons
+          const button = document.createElement('button');
+          button.className = 'my-buttons';  // Add class for easy selection
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
   },
   {
     name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store."
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square", "Level Up!"],
+    "button functions": [buyHealth, buyWeapon, goTown, levelUp],
+    text: "You enter the store.",
+    "button visibility": [true, true, true, true],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
   },
   {
     name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters."
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square", "Level Up!"],
+    "button functions": [fightSlime, fightBeast, goTown, levelUp],
+    text: "You enter the cave. You see some monsters.",
+    "button visibility": [true, true, true, true],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
   },
   {
     name: "fight",
-    "button text": ["Attack", "Dodge", "Run"],
-    "button functions": [attack, dodge, goTown],
-    text: "You are fighting a monster."
+    "button text": ["Attack", "Dodge", "Run", "Level Up!"],
+    "button functions": [attack, dodge, goTown, levelUp],
+    text: "You are fighting a monster.",
+    "button visibility": [true, true, true, false],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
   },
   {
     name: "kill monster",
-    "button text": ["Go to town square", "Go to town square", "Go to town square"],
-    "button functions": [goTown, goTown, easterEgg],
-    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    "button text": ["Go to town square", "Go to town square", "Go to town square", "Go to town square"],
+    "button functions": [goTown, goTown, easterEgg, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+    "button visibility": [true, true, true, false],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
   },
   {
     name: "lose",
-    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-    "button functions": [restart, restart, restart],
-    text: "You die. &#x2620;"
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart, restart],
+    text: "You die. &#x2620;",
+    "button visibility": [true, true, true, true],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
   },
   { 
     name: "win", 
-    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
-    "button functions": [restart, restart, restart], 
-    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?", "REPLAY?"], 
+    "button functions": [restart, restart, restart, restart], 
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
+    "button visibility": [true, true, true, true],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
   },
   {
     name: "easter egg",
-    "button text": ["2", "8", "Go to town square?"],
-    "button functions": [pickTwo, pickEight, goTown],
-    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
-  }
+    "button text": ["2", "8", "Go to town square?", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+    "button visibility": [true, true, true, false],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
+  },
+  {
+    name: "levelUp",
+    "button text": ["Go to town square", "Go to town square", "Go to town square", "Level Up!"],
+    "button functions": [goTown, goTown, goTown, levelUp],
+    text: "Would you like to trade some xp to level up your attack?",
+    "button visibility": [true, false, false, true],
+    hideButton(index) {
+      this["button visibility"][index] = false;
+      this.renderButtons();
+    },
+    showButton(index) {
+      this["button visibility"][index] = true;
+      this.renderButtons();
+    },
+    renderButtons() {
+      const container = document.getElementById('button-container');
+      container.innerHTML = '';
+      this["button text"].forEach((text, index) => {
+        if (this["button visibility"][index]) {
+          const button = document.createElement('button');
+          button.className = 'my-buttons';
+          button.innerText = text;
+          button.onclick = this["button functions"][index];
+          container.appendChild(button);
+        }
+      });
+    }
+  },
 ];
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+button4.onclick = levelUp;
 
-function update(location) {
+function updateMonsterAndTextDisplay(location) {
   monsterStats.style.display = "none";
-  button1.innerText = location["button text"][0];
-  button2.innerText = location["button text"][1];
-  button3.innerText = location["button text"][2];
-  button1.onclick = location["button functions"][0];
-  button2.onclick = location["button functions"][1];
-  button3.onclick = location["button functions"][2];
   text.innerHTML = location.text;
 }
 
 function goTown() {
-  update(locations[0]);
+  updateMonsterAndTextDisplay(locations[0]);
+  locations[0].renderButtons();
 }
 
 function goStore() {
-  update(locations[1]);
+  updateMonsterAndTextDisplay(locations[1]);
+  locations[1].renderButtons();
 }
 
 function goCave() {
-  update(locations[2]);
+  updateMonsterAndTextDisplay(locations[2]);
+  locations[2].renderButtons();
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+function levelUp() {
+  if (playerLevel >= maxLevel) {
+    text.innerText = "You have reached the maximum level!";
+    return;
+  }
+
+  if (xp >= xpNeeded) {
+    xp -= xpNeeded;
+    xpText.innerText = xp;
+
+    playerLevel++;
+    playerLevelText.innerText = playerLevel;
+    
+    text.innerText = `Congratulations, you are now Level ${playerLevel}!`;
+
+    // Increase the power of weapons with each level
+    for (let i = 0; i < weapons.length; i++) {
+      weapons[i].power *= 1.5;
+    }
+
+    // Increase the XP needed for the next level
+    xpNeeded = Math.floor(xpNeeded * 1.5); // Example increment logic
+
+    // Update the UI or display the required XP for the next level if needed
+    
+  } else {
+    text.innerText = `You don't have enough XP.
+    XP needed: ${xpNeeded}`;
+  }
 }
 
 function buyHealth() {
@@ -177,7 +424,8 @@ function fightDragon() {
 }
 
 function goFight() {
-  update(locations[3]);
+  updateMonsterAndTextDisplay(locations[3]);
+  locations[3].renderButtons();
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
   monsterName.innerText = monsters[fighting].name;
@@ -189,7 +437,7 @@ function attack() {
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
   if (isMonsterHit()) {
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
   } else {
     text.innerText += " You miss.";
   }
@@ -229,15 +477,18 @@ function defeatMonster() {
   xp += monsters[fighting].level;
   goldText.innerText = gold;
   xpText.innerText = xp;
-  update(locations[4]);
+  updateMonsterAndTextDisplay(locations[4]);
+  locations[4].renderButtons();
 }
 
 function lose() {
-  update(locations[5]);
+  updateMonsterAndTextDisplay(locations[5]);
+  locations[5].renderButtons();
 }
 
 function winGame() {
-  update(locations[6]);
+  updateMonsterAndTextDisplay(locations[6]);
+  locations[6].renderButtons();
 }
 
 function restart() {
@@ -249,11 +500,15 @@ function restart() {
   goldText.innerText = gold;
   healthText.innerText = health;
   xpText.innerText = xp;
+  playerLevel = 1;
+  playerLevelText.innerText = 1;
+  xpNeeded = 15;
   goTown();
 }
 
 function easterEgg() {
-  update(locations[7]);
+  updateMonsterAndTextDisplay(locations[7]);
+  locations[7].renderButtons();
 }
 
 function pickTwo() {
